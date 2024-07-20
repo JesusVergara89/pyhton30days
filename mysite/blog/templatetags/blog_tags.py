2a84f0ea9d8682eedfee1b26268e13094a912c7c
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-07-20 15:27:06 -0500

    atgs and views about other recent post and also inscrip html

---
+++ mysite/blog/templatetags/blog_tags.py
@@ -0,0 +1,18 @@
+from django import template
+from ..models import Post
+from django.utils.safestring import mark_safe
+import markdown
+
+register = template.Library()
+@register.simple_tag
+def total_post():
+    return Post.published.count()
+
+@register.inclusion_tag('blog/post/latest_post.html')
+def show_latest_post(count=5):
+    latest_post = Post.published.order_by('-publish')[:count]
+    return {'latest_post': latest_post}
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
