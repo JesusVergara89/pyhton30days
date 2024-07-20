b557848e89469d37baa68f19d6effdeab5069575
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-07-19 20:46:15 -0500

    post and detalis

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,11 @@ from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import User
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+            .filter(status=Post.Status.PUBLISHED)
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAF = 'DF','Draft'
@@ -18,6 +23,8 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAF)
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
     class Meta:
         ordering = ['-publish']
         indexes = [
@@ -25,3 +32,5 @@ class Post(models.Model):
         ]
     def __str__(self):
         return self.title
+
+
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    post = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': post})
+
+def post_detail(request, id):
+    try:
+        post = Post.published.get(id=id)
+    except:
+        raise Http404("No post found.")
+    #inste of putting the try except block we can use a get_object_or_404(Post, id = id, status=Post.status.published)
+    return render(request, 'blog/post/detail.html', {'post': post})
+
+
